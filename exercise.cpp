b5554f0a3317066f29e763460c7efb360e4a109d
kannchp <139094458+kannchp@users.noreply.github.com>
2023-07-17 10:15:14 +0700

    Add files via upload

---
+++ exercise.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{ 
+    string Name;
+	int Yearofbirth;
+	cout << "Enter name : ";
+	cin  >> Name;
+	cout << "Enter Year of birth :";
+	cin  >> Yearofbirth;
+	cout << " " << Yearofbirth << "2566-Yearofbirth :" << endl;
+	cout << "Your Age " << 2566-Yearofbirth << " years old." <<endl;
+    system("pause");
+    return(0);
+}
