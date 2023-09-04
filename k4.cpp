90bddabe1fb6f0651e9ec2f3576846f96c03958f
kannchp <139094458+kannchp@users.noreply.github.com>
2023-09-04 12:03:06 +0700

    Add files via upload

--- k4.cpp
+++ k4.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
+#include <string>
 using namespace std;
+void ParseName(string &FirstName,string &LastName, const string FullName);
+void check_Char(string FirstName);
+void check_Char(string LastName);
 int main()
-{ 
-	int c,score;
-	cout << "Input C++ Score : ";
-	cin >> c;
-	cout <<"You "<<((c>=50)? "pass ":"fail ")<< "with score " << c <<endl;
+{
+	string Name = "Stroustrup, Bjarne";
+	string LastName, FirstName;
+	ParseName(FirstName,LastName,Name);
+	Name = FirstName + " " + LastName;
+	cout << "Name : " << Name << endl;
 	system("pause");
-	return (0);
+	return(0);
+}
+void ParseName(string &FirstName,string &LastName, const string FullName)
+{
+	int I = FullName.find(",");
+	LastName = FullName.substr(0,I);
+	FirstName = FullName.substr(I+2,FullName.size());
+}
+void check_Char(string FirstName)
+{	int lower=0, upper =0, special=0;
+	for(int i = 0;i<FirstName.length()-1;i++)
+	{ cout<<FirstName.at(i)<<endl;
+      if(islower(FirstName.at(i))) lower++;
+	  else if (isupper(FirstName.at(i))) upper++;
+	  else special++;
+	} 
+	cout<<"Lower Character = "<<lower<<endl;
+	cout<<"Upper Character = "<<upper<<endl;
+	cout<<"Special Character = "<<special<<endl;
 }
