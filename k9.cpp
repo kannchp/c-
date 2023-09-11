7bee1d42445f9c9ca28303d5abbf30bd0a4864f8
kannchp <139094458+kannchp@users.noreply.github.com>
2023-09-11 09:23:03 +0700

    Add files via upload

--- k9.cpp
+++ k9.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int sum(int x,int y, int z=0, int w=0)
-{
-return(x+y+z+w);
+int sum(int x,int y)
+{	return(x+y);
+}
+int sum(double x, double y)
+{	return(x+y);
 }
 int main()
 {	cout<<sum(10,15)<<endl;
-	cout<<sum(10,15,25)<<endl;
-	cout<<sum(10,15,25,30)<<endl;
+	cout<<sum(25.25,10.45)<<endl;
 	system("pause");
 	return 0;
 }
