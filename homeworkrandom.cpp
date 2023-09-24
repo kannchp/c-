1e29dcdb6a3a3ae0f1dba6b2876d157a46d67963
kannchp <139094458+kannchp@users.noreply.github.com>
2023-09-24 20:45:35 +0700

    Add files via upload

---
+++ homeworkrandom.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	cout << "Element Value Histogram" << endl;
+	cout << endl;
+	int num[10];
+	srand(time(NULL));
+	for(int i = 0; i < 10;i++)
+	{
+		num[i] = 1+rand()%29;
+		cout<<"  "<<i<<"   "<<setw(2)<< num[i]<<" ";
+
+		for(int n = 0 ; n < num[i] ;n++)
+		{
+			cout << "*" ;
+		}
+		cout << endl;
+	
+	}
+	return(0);
+}
