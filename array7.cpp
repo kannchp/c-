7bd1639de08feda1e9ddb259610ff88887981461
kannchp <139094458+kannchp@users.noreply.github.com>
2023-10-02 11:52:04 +0700

    Add files via upload

---
+++ array7.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+int main()
+{    int num[3][3];
+    string name[3];
+    for(int i=0;i<3;i++)
+    {    cout<<"Student "<<i+1<<endl;
+        cout<<"Enter Name ["<<i+1<<"] :";
+        cin>>name[i];
+        for(int j=0;j<3;j++)
+        {    cout<<"Enter Score  ["<<i<<"]["<<j<<"] :" ;
+            cin>>num[i][j];
+        }
+    }
+    cout<<"Show Scores"<<endl;
+    cout<<setfill('-')<<setw(40)<<""<<endl;
+    cout<<"Student\tMath\tScience\tEnglish\n";
+    cout<<setfill('-')<<setw(40)<<""<<endl;
+    for(int i=0;i<3;i++)
+    {    cout<<i+1<<" "<<name[i]<<"\t";
+        for(int j=0;j<3;j++)
+        {    cout<<num[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+    system("pause");
+    return 0;
+}
